b6fbffc6943131bda6998fe578604de2d8ef1fab
divyaB32 <divyaboomibalakan@gmail.com>
2026-03-03 22:51:53 +0530

    Add CSS files

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./AdminDashboard.css";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const SERIES = [
   "Fabula Series",
